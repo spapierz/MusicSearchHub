7e53d6fb92dc91cc9ac60df84118e951f575b0e9
Sarah Papierz <sarah.papierz@gmail.com>
2023-08-22 17:26:02 -0500

    cleanup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import FavoritesButton from './FavoritesButton';
@@ -15,13 +15,23 @@ const logoStyles = {
 }
 
 const Header: React.FC = () => {
-  const history = useHistory();
   const logo = '/images/genreGaze-logo.png';
 
   return (
     <Grid container sx={containerStyles}>
       <Grid item xs={6}>
-        <img style={logoStyles} src={logo} onClick={e => history.push('/')} alt='Back to (Home) genre search' width="200px" height="100px" />
+      <img
+        src={logo}
+        alt="Back to (Home) genre search"
+        width="200px"
+        height="100px"
+        style={{
+          maxWidth: '150px',
+          height: 'auto',
+          padding: '3rem 0px 2rem',
+          cursor: 'pointer'
+        }}
+      />
       </Grid>
       <Grid item xs={6}>
         <FavoritesButton />
